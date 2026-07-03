81d206b115853af1db417b4543b8b39e95a62619
Maryammmy <maryamsameh1610@gmail.com>
2026-07-03 05:08:09 +0200

    remove wwwfigma.com in the next config

--- next.config.ts
+++ next.config.ts
@@ -10,11 +10,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "www.figma.com",
-        pathname: "/api/mcp/asset/**",
-      },
-      {
-        protocol: "https",
         hostname: "cozybakesinc.apianca.online",
         pathname: "/**",
       },
